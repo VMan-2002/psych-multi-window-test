e8adacc81c598dd50cd5a372827b9f147ca9e447
VMan-2002 <vman.2002.au@gmail.com>
2022-10-02 15:36:22 +1100

    Cod

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -74,6 +74,17 @@ import sys.io.File;
 import vlc.MP4Handler;
 #end
 
+import lime.app.Application;
+import lime.graphics.RenderContext;
+import lime.ui.MouseButton;
+import lime.ui.KeyCode;
+import lime.ui.KeyModifier;
+import lime.ui.Window;
+import openfl.geom.Matrix;
+import openfl.geom.Rectangle;
+import openfl.display.Sprite;
+import openfl.utils.Assets;
+
 using StringTools;
 
 class PlayState extends MusicBeatState
@@ -323,6 +334,10 @@ class PlayState extends MusicBeatState
 	public static var lastCombo:FlxSprite;
 	// stores the last combo score objects in an array
 	public static var lastScore:Array<FlxSprite> = [];
+	
+	var windowDad:Window;
+	var dadWin = new Sprite();
+	var dadScrollWin = new Sprite();
 
 	override public function create()
 	{
@@ -829,6 +844,14 @@ class PlayState extends MusicBeatState
 				if(!ClientPrefs.lowQuality) foregroundSprites.add(new BGSprite('tank4', 1300, 900, 1.5, 1.5, ['fg']));
 				foregroundSprites.add(new BGSprite('tank5', 1620, 700, 1.5, 1.5, ['fg']));
 				if(!ClientPrefs.lowQuality) foregroundSprites.add(new BGSprite('tank3', 1300, 1200, 3.5, 2.5, ['fg']));
+			case 'transp':
+				var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.fromRGB(1, 1, 1));
+				if (defaultCamZoom < 1) {
+					bg.scale.scale(1 / defaultCamZoom);
+				}
+				bg.scrollFactor.set();
+				add(bg);
+				FlxTransWindow.getWindowsTransparent();
 		}
 
 		switch(Paths.formatToSongPath(SONG.song))
@@ -1388,6 +1411,10 @@ class PlayState extends MusicBeatState
 		Paths.clearUnusedMemory();
 		
 		CustomFadeTransition.nextCamera = camOther;
+		
+		if (curStage == 'transp') {
+			popupWindow(1280, 720, 500, 'Testing Testing');
+		}
 	}
 
 	#if (!flash && sys)
@@ -3309,6 +3336,19 @@ class PlayState extends MusicBeatState
 		setOnLuas('cameraY', camFollowPos.y);
 		setOnLuas('botPlay', cpuControlled);
 		callOnLuas('onUpdatePost', [elapsed]);
+		
+		if (curStage == 'transp') {
+			@:privateAccess
+			var dadFrame = dad._frame;
+			
+			if (dadFrame == null || dadFrame.frame == null) return; // prevents crashes (i think???)
+				
+			var rect = new Rectangle(dadFrame.frame.x, dadFrame.frame.y, dadFrame.frame.width, dadFrame.frame.height);
+			
+			dadScrollWin.scrollRect = rect;
+			dadScrollWin.x = (((dadFrame.offset.x) - (dad.offset.x / 2)) * dadScrollWin.scaleX);
+			dadScrollWin.y = (((dadFrame.offset.y) - (dad.offset.y / 2)) * dadScrollWin.scaleY);        
+		}
 	}
 
 	function openPauseMenu()
@@ -5308,4 +5348,62 @@ class PlayState extends MusicBeatState
 
 	var curLight:Int = -1;
 	var curLightEvent:Int = -1;
+	
+	function popupWindow(customWidth:Int, customHeight:Int, ?customX:Int, ?customName:String) {
+		var display = Application.current.window.display.currentMode;
+		// PlayState.defaultCamZoom = 0.5;
+
+		if(customName == '' || customName == null){
+			customName = 'Opponent.json';
+		}
+
+		windowDad = Lib.application.createWindow({
+			title: customName,
+			width: customWidth,
+			height: customHeight,
+			borderless: false,
+			alwaysOnTop: true
+
+		});
+		if(customX == null){
+			customX = -10;
+		}
+		windowDad.x = customX;
+			windowDad.y = Std.int(display.height / 2);
+		windowDad.stage.color = 0xFF010101;
+		@:privateAccess
+		windowDad.stage.addEventListener("keyDown", FlxG.keys.onKeyDown);
+		@:privateAccess
+		windowDad.stage.addEventListener("keyUp", FlxG.keys.onKeyUp);
+		// Application.current.window.x = Std.int(display.width / 2) - 640;
+		// Application.current.window.y = Std.int(display.height / 2);
+
+		// var bg = Paths.image(PUT YOUR IMAGE HERE!!!!).bitmap;
+		// var spr = new Sprite();
+
+		var m = new Matrix();
+
+		// spr.graphics.beginBitmapFill(bg, m);
+		// spr.graphics.drawRect(0, 0, bg.width, bg.height);
+		// spr.graphics.endFill();
+		FlxG.mouse.useSystemCursor = true;
+
+		//Application.current.window.resize(640, 480);
+
+
+
+		dadWin.graphics.beginBitmapFill(dad.pixels, m);
+		dadWin.graphics.drawRect(0, 0, dad.pixels.width, dad.pixels.height);
+		dadWin.graphics.endFill();
+		dadScrollWin.scrollRect = new Rectangle();
+	// windowDad.stage.addChild(spr);
+		windowDad.stage.addChild(dadScrollWin);
+		dadScrollWin.addChild(dadWin);
+		dadScrollWin.scaleX = 0.7;
+		dadScrollWin.scaleY = 0.7;
+		// dadGroup.visible = false;
+		// uncomment the line above if you want it to hide the dad ingame and make it visible via the windoe
+		Application.current.window.focus();
+			FlxG.autoPause = false;
+    }
 }
